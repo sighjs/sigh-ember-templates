2acd02520b8a493ee9d6b842e0fa79f162b6fd78
James Pike <jpike@chilon.net>
2014-12-07 21:56:43 +0000

    Initial commit.

---
+++ index.js
@@ -0,0 +1,23 @@
+var compiler = require('ember-template-compiler')
+
+module.exports = function(opts) {
+  if (! opts)
+    opts = {}
+
+  return function(operation) {
+    return operation.inputs.map(function(resource) {
+      var modulePath = resource.filePath
+      modulePath = modulePath.replace(/\.js$/, '')
+      if (opts.getModulePath)
+        modulePath = opts.getModulePath(modulePath)
+
+      // TODO: source maps
+      var output = compiler.precompile(resource.data).toString()
+
+      resource.data = 'define("' + modulePath + '", ["ember", "exports"], function(__dependency1__, __exports__) {\n  "use strict";\n  var Ember = __dependency1__["default"];\n  __exports__["default"] = Ember.Handlebars.template(' + output + ');\n});\n';
+
+      return resource
+
+    })
+  }
+}
